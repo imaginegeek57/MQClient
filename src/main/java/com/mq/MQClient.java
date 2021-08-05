2ca8c51c643d1d45004f66427ed8f2bb51bb4721
Vladimir <imaginegeek57@gmail.com>
2021-08-05 08:46:40 +0300

    improve

--- src/main/java/com/mq/MQClient.java
+++ src/main/java/com/mq/MQClient.java
@@ -3,26 +3,33 @@ package com.mq;
 import com.ibm.jms.JMSMessage;
 import com.ibm.jms.JMSTextMessage;
 import com.ibm.mq.jms.*;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import javax.jms.JMSException;
 import javax.jms.QueueSender;
 import javax.jms.Session;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
 
 public class MQClient {
     private static final Logger log = LogManager.getLogger(MQClient.class);
 
+
     public static MQQueueSession getSession() throws JMSException {
         MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
         // docker image: ibmcom/mq:9.1.1.0
-        connectionFactory.setHostName("localhost");
-        connectionFactory.setPort(1414);
-        connectionFactory.setQueueManager("QM1");
-        connectionFactory.setChannel("DEV.ADMIN.SVRCONN");
+        connectionFactory.setHostName("s-msk-v-mq01.raiffeisen.ru");
+        connectionFactory.setPort(1415);
+        connectionFactory.setQueueManager("QMDEV01");
+        connectionFactory.setChannel("QAS.DEV01");
         connectionFactory.setTransportType(1);
 
-        MQQueueConnection connection = (MQQueueConnection) connectionFactory.createQueueConnection("admin", "passw0rd");
+        MQQueueConnection connection = (MQQueueConnection) connectionFactory.createQueueConnection();
         connection.start();
         return (MQQueueSession) connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
     }
@@ -47,4 +54,78 @@ public class MQClient {
         receiver.close();
     }
 
+    public Cookies authRestTest() {
+        Map<String, String> auth = new HashMap<>();
+        auth.put("username", "");
+        auth.put("password", "");
+
+        return given()
+                .relaxedHTTPSValidation()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(auth)
+                .post("https://s-msk-v-mq01.raiffeisen.ru:9443/ibmmq/rest/v1/login")
+                .then()
+                .statusCode(204)
+                .extract()
+                .response()
+                .getDetailedCookies();
+    }
+
+    public void checkRestTest() {
+        given()
+                .relaxedHTTPSValidation()
+                .contentType(ContentType.JSON)
+                .cookie(String.valueOf(authRestTest()))
+                .when()
+                .get("https://s-msk-v-mq01.raiffeisen.ru:9443/ibmmq/rest/v1/ruamdva/qmgr/QMWEB01/queue")
+                .then()
+                .statusCode(200);
+    }
+
+    public void createQueueRestTest(String queueName) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("ibm-mq-rest-csrf-token", "v");
+        Map<String, String> body = new HashMap<>();
+        body.put("name", queueName);
+
+        given()
+                .relaxedHTTPSValidation()
+                .contentType(ContentType.JSON)
+                .cookie(String.valueOf(authRestTest()))
+                .headers(headers)
+                .body(body)
+                .when()
+                .post("https://s-msk-v-mq01.raiffeisen.ru:9443/ibmmq/rest/v1/ruamdva/qmgr/QMDEV01/queue/")
+                .then()
+                .statusCode(201);
+    }
+
+    public void deleteQueueRestTest(String queueName) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("ibm-mq-rest-csrf-token", "v");
+
+        given()
+                .relaxedHTTPSValidation()
+                .cookie(String.valueOf(authRestTest()))
+                .headers(headers)
+                .when()
+                .delete("https://s-msk-v-mq01.raiffeisen.ru:9443/ibmmq/rest/v1/ruamdva/QMDEV01/queue/" + queueName + "?purge")
+                .then()
+                .statusCode(204);
+    }
+
+    public void clearQueueRestTest(String queueName) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("ibm-mq-csrf-token", "v");
+
+        given()
+                .relaxedHTTPSValidation()
+                .cookie(String.valueOf(authRestTest()))
+                .headers(headers)
+                .when()
+                .delete("https://localhost:9443/ibmmq/console/internal/ibmmq/qmgr/QM1/queue/" + queueName + "/messages")
+                .then()
+                .statusCode(200);
+    }
 }
--- src/test/java/com/mq/MQClientTest.java
+++ src/test/java/com/mq/MQClientTest.java
@@ -17,103 +17,38 @@ class MQClientTest {
 //    public GenericContainer mqContainer = new GenericContainer(DockerImageName.parse("ibmcom/mq:9.1.1.0"))
 //            .withExposedPorts(1414);
 
-    private MQClient mqClient = new MQClient();
+    private final MQClient mqClient = new MQClient();
 
 
     @Test
     public void sendMessageTest() throws JMSException {
-        createQueueRestTest("test.queue");
+//        mqClient.createQueueRestTest("TEST.QUEUE.TF2");
 
-        mqClient.send("some test msg 1", "test.queue");
-        mqClient.send("some test msg 2", "test.queue");
-        mqClient.send("some test msg 3", "test.queue");
+        mqClient.send("some test msg 1", "TEST.QUEUE.TF");
+        mqClient.send("some test msg 2", "TEST.QUEUE.TF");
+        mqClient.send("some test msg 3", "TEST.QUEUE.TF");
+        mqClient.send("some test msg 4", "TEST.QUEUE.TF");
 
-        mqClient.getMessages("test.queue");
-        mqClient.getMessages("test.queue");
+        mqClient.getMessages("TEST.QUEUE.TF");
+        mqClient.getMessages("TEST.QUEUE.TF");
+//        mqClient.getMessages("TEST.QUEUE.TF");
+//        mqClient.getMessages("TEST.QUEUE.TF");
+//        mqClient.getMessages("TEST.QUEUE.TF");
+    }
+
+    @Test
+    public void checkConnection() {
+        mqClient.checkRestTest();
     }
 
     @Test
     public void clearQueueTest() {
-        clearQueueRestTest("test.queue");
+        mqClient.clearQueueRestTest("test.queue");
     }
 
     @Test
     public void deleteQueueTest() {
-        deleteQueueRestTest("test.queue");
+        mqClient.deleteQueueRestTest("TEST.QUEUE.TF");
     }
 
-    public Cookies authRestTest() {
-        Map<String, String> auth = new HashMap<>();
-        auth.put("username", "admin");
-        auth.put("password", "passw0rd");
-
-        return RestAssured.given()
-                .relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .when()
-                .body(auth)
-                .post("https://localhost:9443/ibmmq/rest/v2/login")
-                .then()
-                .statusCode(204)
-                .extract()
-                .response()
-                .getDetailedCookies();
-    }
-
-    public void checkRestTest() {
-        given()
-                .relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .cookie(String.valueOf(authRestTest()))
-                .when()
-                .get("https://localhost:9443/ibmmq/rest/v1/admin/qmgr")
-                .then()
-                .statusCode(200);
-    }
-
-    public void createQueueRestTest(String queueName) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("ibm-mq-rest-csrf-token", "v");
-        Map<String, String> body = new HashMap<>();
-        body.put("name", queueName);
-
-        given()
-                .relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .cookie(String.valueOf(authRestTest()))
-                .headers(headers)
-                .body(body)
-                .when()
-                .post("https://localhost:9443/ibmmq/rest/v1/admin/qmgr/QM1/queue/")
-                .then()
-                .statusCode(201);
-    }
-
-    public void deleteQueueRestTest(String queueName) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("ibm-mq-rest-csrf-token", "v");
-
-        given()
-                .relaxedHTTPSValidation()
-                .cookie(String.valueOf(authRestTest()))
-                .headers(headers)
-                .when()
-                .delete("https://localhost:9443/ibmmq/rest/v1/admin/qmgr/QM1/queue/" + queueName + "?purge")
-                .then()
-                .statusCode(204);
-    }
-
-    public void clearQueueRestTest(String queueName) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("ibm-mq-csrf-token", "v");
-
-        given()
-                .relaxedHTTPSValidation()
-                .cookie(String.valueOf(authRestTest()))
-                .headers(headers)
-                .when()
-                .delete("https://localhost:9443/ibmmq/console/internal/ibmmq/qmgr/QM1/queue/" + queueName + "/messages")
-                .then()
-                .statusCode(200);
-    }
 }
